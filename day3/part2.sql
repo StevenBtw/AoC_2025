75b8310c3810941fefde69dd85b3a9462b92e9b4
StevenBtw <stevengrond@gmail.com>
2025-12-03 21:32:44 +0100

    Greed(y) is good

--- day3/part2.sql
+++ day3/part2.sql
@@ -1,63 +1,51 @@
-WITH RECURSIVE position_generator AS (
-    SELECT 
+WITH RECURSIVE greedy_pick AS (
+    SELECT
         line_number,
         line_data,
-        1 as pos1
-    FROM banks 
+        0 as pick_num,
+        ''::VARCHAR as result_value,
+        1 as search_start
+    FROM banks
     WHERE LENGTH(line_data) >= 12
-    
+
     UNION ALL
-    
-    SELECT 
+
+    SELECT
         line_number,
         line_data,
-        pos1 + 1
-    FROM position_generator
-    WHERE pos1 < LENGTH(line_data)
-),
-
-all_chars AS (
-    SELECT 
-        line_number,
-        line_data,
-        pos1,
-        SUBSTR(line_data, pos1, 1) as char1
-    FROM position_generator
-),
-
-three_picks AS (
-    SELECT 
-        c1.line_number,
-        c1.line_data,
-        c1.pos1 as exclude_pos1,
-        c2.pos1 as exclude_pos2,
-        c3.pos1 as exclude_pos3
-    FROM all_chars c1
-    JOIN all_chars c2 
-        ON c1.line_number = c2.line_number 
-        AND c1.pos1 < c2.pos1
-    JOIN all_chars c3 
-        ON c2.line_number = c3.line_number 
-        AND c2.pos1 < c3.pos1
-),
-
-all_combinations AS (
-    SELECT 
-        t.line_number,
-        CAST(STRING_AGG(c.char1, '' ORDER BY c.pos1) AS BIGINT) as result_value
-    FROM three_picks t
-    JOIN all_chars c 
-        ON t.line_number = c.line_number
-        AND c.pos1 NOT IN (t.exclude_pos1, t.exclude_pos2, t.exclude_pos3)
-    GROUP BY t.line_number, t.exclude_pos1, t.exclude_pos2, t.exclude_pos3
-    HAVING LENGTH(STRING_AGG(c.char1, '')) = 12
+        pick_num + 1,
+        result_value || best_char,
+        best_pos + 1
+    FROM (
+        SELECT
+            g.line_number,
+            g.line_data,
+            g.pick_num,
+            g.result_value,
+            g.search_start,
+            (
+                SELECT MAX(SUBSTR(g.line_data, pos, 1))
+                FROM generate_series(g.search_start, LENGTH(g.line_data) - (11 - g.pick_num)) as t(pos)
+            ) as best_char,
+            (
+                SELECT MIN(pos)
+                FROM generate_series(g.search_start, LENGTH(g.line_data) - (11 - g.pick_num)) as t(pos)
+                WHERE SUBSTR(g.line_data, pos, 1) = (
+                    SELECT MAX(SUBSTR(g.line_data, pos2, 1))
+                    FROM generate_series(g.search_start, LENGTH(g.line_data) - (11 - g.pick_num)) as t2(pos2)
+                )
+            ) as best_pos
+        FROM greedy_pick g
+        WHERE g.pick_num < 12
+    )
 ),
 
 line_results AS (
-    SELECT 
+    SELECT
         line_number,
-        MAX(result_value) as max_joltage
-    FROM all_combinations
+        MAX(CAST(result_value AS BIGINT)) as max_joltage
+    FROM greedy_pick
+    WHERE pick_num = 12
     GROUP BY line_number
 )
 
