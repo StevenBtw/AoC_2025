4e247e54078d0cb93097b7599aea6b3929aea5b0
Steven Bytheway <38373784+StevenBtw@users.noreply.github.com>
2025-12-04 06:39:47 +0100

    Cleaned up part1.m
    Removed spaces

--- day4/part1.m
+++ day4/part1.m
@@ -2,12 +2,8 @@ lines = readlines("input.txt");
 H = numel(lines);
 W = strlength(lines(1));
 G = char(lines); 
-
 M = (G == '@');  
-
 K = ones(3); 
 neighbor_counts = conv2(double(M), K, 'same') - M;
-
-
 accessible_mask = M & (neighbor_counts < 4);
 answer = nnz(accessible_mask);
