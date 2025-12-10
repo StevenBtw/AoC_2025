8030d8d328c850c2e98745d3f1c9302f5bedae7b
StevenBtw <stevengrond@gmail.com>
2025-12-10 22:39:39 +0100

    CUDA Solution

--- README.md
+++ README.md
@@ -48,7 +48,7 @@ A different tool (software, language or library) for each day.
 | [7](day07/) | Rust | I may have been less rusty (or more) than I thought, initially wanted to go with a hashmap, but instead went with a simple set/vector solution for both 1 & 2. | Medium | 25/38 |
 | [8](day0/)   | NetworkX   | Happy with my networkx pick, used unionfind(kruskals) for both part 1 & 2, stop at 1000 edges for 1 and when connected for 2. | Easy | 12/19 |
 | [9](day09/)   | OR-Tools | Part 1 was ok, would have been a lot easier with itertools instead of ortools. Feeding the solver the right constraints for part 2 was a challenge. Used the CP-SAT Solver and the python documentation is excellent. | Hard | 39/68 |
-| [10](day10/)  | CUDA | Ofcourse the perfect solver problem comes the day after shoehorning my solver pick. Bruteforced part 1, GPU didn't even sweat, the challenge was reading comprehension. | Hard | 135/? |
+| [10](day10/)  | CUDA | Ofcourse the perfect solver problem comes the day after shoehorning my solver pick. Bruteforced part 1, GPU didn't even sweat, the challenge was reading comprehension. Part 2 was not bruteforcable so I tried different ways of reducing the search space before feeding it to the GPU, tried DFS, getting close with a genetic algo on CUDA before bruteforcing the space around the results from the algo, but in the end a pure GPU solution with a gaussian elimination step in the kernel worked.  | Hard | 135/>6h |
 | [11](day11/)  | ?    |           |            |         |
 | [12](day12/)  | ?    |           |            |         |
 
@@ -85,6 +85,11 @@ A different tool (software, language or library) for each day.
 
 ![alt text](images/d9p2.png)
 
-> I know the OR-Tools pick is not ideal, but sometimes you have to find a problem for your solution instead of a solution for your problem.
+> I know the OR-Tools pick is not ideal, but sometimes you have to find a problem for your solution instead of a solution for your problem. (aged like milk)
 
+### Day 10 - CUDA 
+
+![alt text](images/brrrrr.png)
+
+> GPU goes BRRRRR, no  
 ---
--- day10/part2.cu
+++ day10/part2.cu
@@ -21,8 +21,6 @@ extern "C" __global__ void solve_machines(
      * - blockDim.x : "number of threads per block"
      */
     int blockId = blockIdx.x;
-    int threadId = threadIdx.x;
-
     // boundary check
     if (blockId >= N) return;
 
@@ -33,95 +31,219 @@ extern "C" __global__ void solve_machines(
     int buttonGroupOffset = button_group_offsets[blockId];
     int maxTarget = max_target_per_machine[blockId];
 
-    /*
-     * __shared__ : "Variables declared with __shared__ qualifier reside in
-     * shared memory of a thread block"
-     */
-    __shared__ int sharedBestResult;
+    __shared__ long long matrix[16][20];
+    __shared__ int pivotCol[16];
+    __shared__ int freeVars[16];
+    __shared__ int numFree;
+    __shared__ int bestResult;
 
-    // thread 0 initializes shared memory
+    int threadId = threadIdx.x;
+    int numCols = numButtons + 1;
+
     if (threadId == 0) {
-        sharedBestResult = 99999;
-    }
+        bestResult = 999999999;
+        numFree = 0;
 
-    // Synchronize to ensure shared memory is populated
-    __syncthreads();
-
-    /*
-     * Local arrays stored in registers/local memory
-     */
-    int targetValues[32];
-    int workingState[32];
-
-    // copy target values from global memory to local memory
-    for (int i = 0; i < numCounters; i++) {
-        targetValues[i] = all_targets[targetOffset + i];
-    }
-
-    /*
-     * Grid-stride loop pattern for work distribution
-     */
-    long long totalCombinations = 1;
-    int base = maxTarget + 1;
-    for (int b = 0; b < numButtons; b++) {
-        totalCombinations *= base;
-        if (totalCombinations > 100000000LL) {
-            totalCombinations = 100000000LL;
-            break;
-        }
-    }
-
-    for (long long combination = threadId; combination < totalCombinations; combination += blockDim.x) {
-
-        // initialize working state to all zeros
         for (int i = 0; i < numCounters; i++) {
-            workingState[i] = 0;
+            for (int j = 0; j < numCols; j++) {
+                matrix[i][j] = 0;
+            }
+            pivotCol[i] = -1;
         }
 
-        // Count number of buttons pressed
-        int buttonPressCount = 0;
-        long long temp = combination;
-
-        // iterate through each button
         for (int b = 0; b < numButtons; b++) {
-            int presses = temp % base;
-            temp /= base;
-            buttonPressCount = buttonPressCount + presses;
-
-            // get indices that this button toggles
             int buttonIndex = buttonGroupOffset + b;
             int indexOffset = button_index_offsets[buttonIndex];
             int indexCount = button_index_counts[buttonIndex];
 
-            // toggle each position
-            for (int j = 0; j < indexCount; j++) {
-                int position = all_button_indices[indexOffset + j];
-                workingState[position] += presses;
+            for (int k = 0; k < indexCount; k++) {
+                int counter = all_button_indices[indexOffset + k];
+                if (counter < numCounters) {
+                    matrix[counter][b] = 1;
+                }
             }
         }
 
-        int isMatch = 1;
         for (int i = 0; i < numCounters; i++) {
-            if (workingState[i] != targetValues[i]) {
-                isMatch = 0;
+            matrix[i][numButtons] = all_targets[targetOffset + i];
+        }
+
+        int h = 0;
+        int k = 0;
+
+        while (h < numCounters && k < numButtons) {
+            int i_max = h;
+            long long maxVal = 0;
+            for (int i = h; i < numCounters; i++) {
+                long long v = matrix[i][k];
+                if (v < 0) v = -v;
+                if (v > maxVal) {
+                    maxVal = v;
+                    i_max = i;
+                }
+            }
+
+            if (maxVal == 0) {
+                k++;
+            } else {
+                for (int j = 0; j < numCols; j++) {
+                    long long tmp = matrix[h][j];
+                    matrix[h][j] = matrix[i_max][j];
+                    matrix[i_max][j] = tmp;
+                }
+
+                pivotCol[h] = k;
+
+                for (int i = 0; i < numCounters; i++) {
+                    if (i == h) continue;
+
+                    long long num = matrix[i][k];
+                    long long denom = matrix[h][k];
+
+                    if (num != 0) {
+                        int needScale = 0;
+                        for (int j = k + 1; j < numCols; j++) {
+                            long long product = num * matrix[h][j];
+                            long long absProd = product < 0 ? -product : product;
+                            long long absDenom = denom < 0 ? -denom : denom;
+                            if (absProd % absDenom != 0) {
+                                needScale = 1;
+                                break;
+                            }
+                        }
+
+                        if (needScale) {
+                            long long absDenom = denom < 0 ? -denom : denom;
+                            for (int j = 0; j < numCols; j++) {
+                                matrix[i][j] *= absDenom;
+                            }
+                            denom = denom < 0 ? -1 : 1;
+                        }
+
+                        for (int j = k + 1; j < numCols; j++) {
+                            matrix[i][j] -= matrix[h][j] * num / denom;
+                        }
+                        matrix[i][k] = 0;
+                    }
+                }
+
+                h++;
+                k++;
             }
         }
 
-        // update best result if this combination also solves
-        if (isMatch == 1) {
-            atomicMin(&sharedBestResult, buttonPressCount);
+        for (int col = 0; col < numButtons; col++) {
+            int hasPivot = 0;
+            for (int row = 0; row < numCounters; row++) {
+                if (pivotCol[row] == col) {
+                    hasPivot = 1;
+                    break;
+                }
+            }
+            if (!hasPivot) {
+                freeVars[numFree++] = col;
+            }
         }
     }
 
-    // synchronize before reading final result
     __syncthreads();
 
-    // thread 0 writes the result to global memory
-    if (threadId == 0) {
-        if (sharedBestResult == 99999) {
-            results[blockId] = 0;
-        } else {
-            results[blockId] = sharedBestResult;
+    int effectiveRows = numCounters;
+    while (effectiveRows > 0) {
+        int allZero = 1;
+        for (int j = 0; j < numCols; j++) {
+            if (matrix[effectiveRows - 1][j] != 0) {
+                allZero = 0;
+                break;
+            }
         }
+        if (allZero) effectiveRows--;
+        else break;
+    }
+
+    if (numFree == 0) {
+        if (threadId == 0) {
+            int ans = 0;
+            int valid = 1;
+            for (int row = 0; row < effectiveRows; row++) {
+                int col = pivotCol[row];
+                if (col < 0) continue;
+
+                long long num = matrix[row][numButtons];
+                long long denom = matrix[row][col];
+
+                if (denom == 0) { valid = 0; break; }
+
+                long long absNum = num < 0 ? -num : num;
+                long long absDenom = denom < 0 ? -denom : denom;
+                if (absNum % absDenom != 0) { valid = 0; break; }
+
+                long long val = num / denom;
+                if (val < 0) { valid = 0; break; }
+
+                ans += (int)val;
+            }
+            if (valid) {
+                bestResult = ans;
+            }
+        }
+    } else {
+        long long maxVal = maxTarget + 1;
+        long long totalCombos = 1;
+        for (int f = 0; f < numFree && f < 4; f++) {
+            totalCombos *= maxVal;
+            if (totalCombos > 100000000LL) {
+                totalCombos = 100000000LL;
+                break;
+            }
+        }
+
+        for (long long combo = threadId; combo < totalCombos; combo += blockDim.x) {
+            int freeVals[4] = {0, 0, 0, 0};
+            long long temp = combo;
+
+            int sumFree = 0;
+            for (int f = 0; f < numFree && f < 4; f++) {
+                freeVals[f] = (int)(temp % maxVal);
+                temp /= maxVal;
+                sumFree += freeVals[f];
+            }
+
+            int valid = 1;
+            int ans = sumFree;
+
+            for (int row = effectiveRows - 1; row >= 0; row--) {
+                int col = pivotCol[row];
+                if (col < 0) continue;
+
+                long long rhs = matrix[row][numButtons];
+
+                for (int f = 0; f < numFree && f < 4; f++) {
+                    rhs -= matrix[row][freeVars[f]] * freeVals[f];
+                }
+
+                long long denom = matrix[row][col];
+                if (denom == 0) { valid = 0; break; }
+
+                long long absRhs = rhs < 0 ? -rhs : rhs;
+                long long absDenom = denom < 0 ? -denom : denom;
+                if (absRhs % absDenom != 0) { valid = 0; break; }
+
+                long long val = rhs / denom;
+                if (val < 0) { valid = 0; break; }
+
+                ans += (int)val;
+            }
+
+            if (valid) {
+                atomicMin(&bestResult, ans);
+            }
+        }
+    }
+
+    __syncthreads();
+
+    if (threadId == 0) {
+        results[blockId] = (bestResult == 999999999) ? 0 : bestResult;
     }
 }
