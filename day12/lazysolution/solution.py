bc32a3ffdcd0c3d8c2f741e2379bc6b60900bc8d
StevenBtw <stevengrond@gmail.com>
2025-12-12 17:17:41 +0100

    OpenVINO Solution

--- day12/lazysolution/solution.py
+++ day12/lazysolution/solution.py
@@ -1,4 +1,4 @@
-parts = open("input.txt").read().split("\n\n")
+parts = open("../input.txt").read().split("\n\n")
 shapes = [p.count("#") for p in parts[:6]]
 ratios = [sum(int(c)*s for c,s in zip(line.split(": ")[1].split(), shapes)) /
           eval(line.split(":")[0].replace("x","*")) for line in parts[6].strip().split("\n")]
